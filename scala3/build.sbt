ce6dd39c62e65336b7068ab390b37a222773dc45
jeogod <hi@jeova.ninja>
2024-07-15 22:50:16 -0300

    add zio + sttp

--- scala3/build.sbt
+++ scala3/build.sbt
@@ -12,12 +12,19 @@ lazy val nativeBuildSettings = Seq(
   nativeImageInstalled := true
 )
 
+val deps = Seq(
+  "dev.zio" %% "zio" % "2.1.6",
+  "com.softwaremill.sttp.client3" %% "zio" % "3.9.7",
+  "com.softwaremill.sttp.client3" %% "core" % "3.9.7"
+)
+
 lazy val root = project
   .in(file("."))
   .enablePlugins(NativeImagePlugin)
   .settings(
     name := "cli-graal",
     version := "0.1.0-SNAPSHOT",
-    scalaVersion := scala3Version
+    scalaVersion := scala3Version,
+    libraryDependencies := deps
   )
   .settings(nativeBuildSettings)
--- scala3/src/main/scala/Main.scala
+++ scala3/src/main/scala/Main.scala
@@ -1,5 +1,27 @@
+import sttp.client3.httpclient.zio.HttpClientZioBackend
+import sttp.client3._
+import sttp.client3.httpclient.zio._
+import zio._
+
+object ZIOApp {
+  val runtime = Runtime.default
+
+  val run = Unsafe.unsafe { implicit unsafe =>
+    runtime.unsafe.run(req.flatMap(r => ZIO.attempt(println(r.body)))).getOrThrowFiberFailure()
+  }
+}
+
+def req =
+  HttpClientZioBackend().flatMap { backend =>
+    val request = basicRequest
+      .response(asStringAlways).get(uri"https://httpbin.org/get")
+    backend.send(request)
+  }
+
+
 @main def Hello(args: String*): Unit =
   println("Hello world!")
   println(msg)
+  ZIOApp.run
 
 def msg = "I was compiled by Scala 3. :)"
