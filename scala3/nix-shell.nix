fe208e807bf33ccacf0c9cd33561b3792f74f238
jeogod <hi@jeova.ninja>
2024-07-18 23:04:41 -0300

    release workflow

--- scala3/nix-shell.nix
+++ scala3/nix-shell.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
   with pkgs.buildPackages; 
-  let graal =graalvmCEPackages.graalvm-ce-musl;
+  let graal = graalvmCEPackages.graalvm-ce-musl;
   in
   pkgs.mkShell {
-    nativeBuildInputs = [ scala_3 sbt graal musl.dev];
+    nativeBuildInputs = [ scala_3 sbt graal musl.dev upx ];
 
     shellHook = ''
       export GRAAL_HOME=${graal}
+      echo 'GRAAL_HOME=${graal}' > .temp_env
     '';
 }
